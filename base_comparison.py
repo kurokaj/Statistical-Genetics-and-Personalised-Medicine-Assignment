c8ac3600f0e27b922c1aa0b76c2649242d5cfcde
kurokaj <43610073+kurokaj@users.noreply.github.com>
2020-04-16 20:52:07 +0300

    Comparing the values to the base-state
    Got it a bit further. Still missing few things

--- base_comparison.py
+++ base_comparison.py
@@ -11,7 +11,7 @@ import numpy as np
 dict_B = np.load("E:/AALTO/Kevät2020/STATISTICAL GENETICS  & PERS. MED/Project/Statistical-Genetics-and-Personalised-Medicine-Assignment/Data_sets/train_B.npz",
                  allow_pickle=True)
 # Data B in numpy format 
-data_B = dict_B['arr_0']
+#data_B = dict_B['arr_0']
 
 print(data_B.shape)
 # Sepsislabel = 41(40) ; Heartrate = 1
@@ -43,24 +43,50 @@ for idx, identity in enumerate(ids):
     
     low = time_of_sepsis-5
     up = time_of_sepsis+5
-    base_before = np.nanmean(roi[0:time_of_sepsis,0])
-    base_after = np.nanmean(roi[time_of_sepsis:,0])
+    base_before = np.nanmean(roi[0:time_of_sepsis,0].astype(float))
+    base_after = np.nanmean(roi[time_of_sepsis:,0].astype(float))
     
     measured_low = roi[low:time_of_sepsis,0]
     measured_high = roi[time_of_sepsis:up,0]
     
     # Replace nan values with base
-    measured_low = np.nan_to_num(measured_low, nan=base_before)
-    measured_high = np.nan_to_num(measured_high, nan=base_after)
+        # Base_after used only when replacing nan values 
+    temp_low = measured_low == np.nan
+    temp_high = measured_high == np.nan
+    measured_low[temp_low] = base_before
+    measured_high[temp_high] = base_after
 
-    measured = np.concatenate(measured_low, measured_high)
+    measured = np.concatenate((measured_low, measured_high))
     
-    # Calculate the mean over all looped patients
+    # Divide by the pre-sepsis basevalue
+    measured = measured/base_before
     
+    # Stack matrix with all values 
+    if idx==0:
+        all_measured = np.expand_dims(measured, axis=0)
+    else:
+        print(all_measured.shape)
+        print(np.expand_dims(measured, axis=0).shape)
+        all_measured = np.concatenate((all_measured, np.expand_dims(measured, axis=0)), axis=0)
+    
+# Calculate the mean over all looped patients
+print(all_measured.shape)
+avg_measures = np.mean(all_measured, axis=1)
+
+print(avg_measures)
 
 # PROBLEM : only takes account those that have nice 5+5h period in data -> wasting data
     # Also should the nan values be changed into something else
+    # ATM stops because some measurement range is (1,6) -> cant concatinate
+    # Still has NAN values in them so can not calculate right mean
 
-avg = sum/counted
-print(avg)
+print("Skipped values" + str(skipped))
+print("Used calculating the average" + str(idx-skipped))
+
+
+
+
+
+
+
 
